9cc5e6f3110df1cb16cf6d22a091049d7ab49e3c
Jahid Joni <jh.joni07@gmail.com>
2021-03-21 15:24:29 +0600

    hosting

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -4,20 +4,22 @@ import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router';
 import './Destination.css'
 import fakeData from '../../fakedata.json'
-import {getCurrentDate} from './Date'
+import { getCurrentDate } from './Date'
 import { Map } from './Map';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faDollarSign } from '@fortawesome/free-solid-svg-icons'
 
-const Destination = () => {      
+const Destination = () => {
     const { register, handleSubmit, errors } = useForm();
     const [search, setSearch] = useState([])
+
     const { id } = useParams();
-    const myRide = fakeData.find(ride=>ride.id == id)
-    const {image, type, capacity, cost} = myRide
+    const myRide = fakeData.find(ride => ride.id == id)
+    const { image, type, capacity, cost } = myRide
     const onSubmit = data => {
-        setSearch(data)     
+        setSearch(data)
     };
+
 
     return (
         <Container className="mt-5">
@@ -27,12 +29,12 @@ const Destination = () => {
                         <h5>&#9872; {search.pickTo}</h5>
                         <h5>&#9873; {search.pickFrom}</h5>
                         <div className="d-flex mt-4">
-                        <h6>{type}</h6>
-                        <h6 className="mx-4"><FontAwesomeIcon icon={faUser}/> {capacity}</h6>
-                        <h6><FontAwesomeIcon icon={faDollarSign}/>{cost}</h6>
+                            <h6>{type}</h6>
+                            <h6 className="mx-4"><FontAwesomeIcon icon={faUser} /> {capacity}</h6>
+                            <h6><FontAwesomeIcon icon={faDollarSign} />{cost}</h6>
                         </div>
-                        <img src={image} alt="" className="car-img"/>
-                       <p>Travel Date: {getCurrentDate()}</p>
+                        <img src={image} alt="" className="car-img" />
+                        <p>Travel Date: {getCurrentDate()}</p>
                     </div>
                     <form className="destination-form" onSubmit={handleSubmit(onSubmit)}>
                         <Form.Label className="d-block mt-2">Pick From</Form.Label>
@@ -40,11 +42,11 @@ const Destination = () => {
                         <Form.Label className="d-block mt-2">Pick From</Form.Label>
                         <input name="pickTo" ref={register({ required: true })} className="form-input" />
                         {errors.pickTo && <span className="error">This field is required</span>}
-                        <input type="submit" className="form-input m-2 d-block"/>
+                        <input type="submit" className="form-input m-2 d-block" />
                     </form>
                 </Col>
                 <Col className="p-3 form ml-2">
-                <Map> </Map>
+                    <Map> </Map>
                 </Col>
             </Row>
         </Container>
